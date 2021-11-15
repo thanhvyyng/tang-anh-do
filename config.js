00e199ab7b7682fc74c44f1dc24a5b8cb34675a5
thanhvyyng <94368516+thanhvyyng@users.noreply.github.com>
2021-11-15 21:20:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Ola!',
+    introDesc: `Có điều này muốn hỏi nè, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Ok',
+    title: 'Thanh Vy có dĩa huông không?😘',
+    desc: 'Khum trả lời hoặc thoát ra là Thanh Vy cjnk đẹp tuỵt vời',
+    btnYes: 'Thanh Vy đáng iêu thặc sự',
+    btnNo: 'Khummmmm chữ m kéo dài =))',
+    question:'Vậy làm ngiu Thanh Vy nhooo',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Đáng iu z ai nỡ từ chối trùi',
+    mess: 'Biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'Còn giờ thì chờ gì nữa mà không inbox  đi nào..',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
